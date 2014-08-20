9a57d1b8753a34a3b3b4c226bef91f8f46a29c02
Shashwat Kumar <shashwatkmr.001@gmail.com>
2014-08-20 15:22:31 +0530

    Empty files created

---
+++ src/main/java/org/redisadmin/controller/Dummy.java
@@ -0,0 +1,7 @@
+package org.redisadmin.controller;
+
+/**
+ * Created by shashwat001 on 20/8/14.
+ */
+public class Dummy {
+}
---
+++ src/main/java/org/redisadmin/model/Dummy.java
@@ -0,0 +1,7 @@
+package org.redisadmin.model;
+
+/**
+ * Created by shashwat001 on 20/8/14.
+ */
+public class Dummy {
+}
